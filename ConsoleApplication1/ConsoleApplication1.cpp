7fba37545d26513df1a5dc2a011cdbb01ed61ec1
dutabrahmana <dutalinggau25@gmail.com>
2024-03-28 09:22:13 +0700

    membuat isi variabel input

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,10 +7,16 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
-void input() {
-	while (true)
-	{
+void input () {
+	while (true) { 
+
+		cout << "Masukan banyaknya elemen pada array: ";	
+		cin >> n;		
+		if (n <= 20)	
+			break;	
+		else {		
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+
 	}
-}
 
 
