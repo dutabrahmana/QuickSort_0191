01ca861d6992de9d02a25195f50b26977aa3de09
dutabrahmana <dutalinggau25@gmail.com>
2024-03-28 09:47:44 +0700

    membuat bebrapa while

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -50,8 +50,13 @@ void q_short(int low, int high)
 
 		pivot = arr[low];
 		i = low + 1;
-		j = high;
+		j = high; 
+		while (i <= j) 
+		{
+			while ((arr[i] <= pivot) && (i <= high))
+			{
 
-	}
-}
+			}
 
+			
+		
